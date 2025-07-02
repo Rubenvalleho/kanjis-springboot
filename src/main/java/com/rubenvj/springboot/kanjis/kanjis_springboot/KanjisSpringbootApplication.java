fef5015279475dac670ae3df22f61e8adcca4cfa
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2025-07-02 18:39:15 +0200

    Commit inicial

---
+++ src/main/java/com/rubenvj/springboot/kanjis/kanjis_springboot/KanjisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rubenvj.springboot.kanjis.kanjis_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjisSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjisSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubenvj/springboot/kanjis/kanjis_springboot/KanjisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenvj.springboot.kanjis.kanjis_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjisSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
